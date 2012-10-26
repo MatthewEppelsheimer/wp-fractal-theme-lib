ded098c36501d7aeb6b267663c9e1466781c7b97
Matthew Eppelsheimer <matthew.eppelsheimer@gmail.com>
2012-10-25 19:08:03 -0700

    Reorganized files. Updated plugin description. Removed hollow activation function. Added todo for deactivation function.

---
+++ fractal/fractal.php
@@ -0,0 +1,148 @@
+<?php
+/*
+Plugin Name: Fractal
+Version: 0.1
+Plugin URI: http://rocketlift.com/software/fractal
+Description: A templating engine for WordPress that supports template heirarchy and infinite nesting
+Author: Matthew Eppelsheimer
+Author URI: http://rocketlift.com/
+License: GPL 2
+*/
+
+// deactivation
+
+/* @todo
+*/
+
+function wp_fractal_deactivate() {
+	/*	Stuff	
+		Such as detecting themes that require this plugin's support, and possibly disable them
+	*/
+}
+
+register_deactivation_hook( __FILE__, 'wp_fractal_deactivate' );
+
+/*
+ *	Core Fractal Setup
+ */
+ 
+/*
+ *	fractal_template_setup()
+ *	@description	Prepares global the Fractal templating engine before loading templates
+ */
+
+function fractal_template_setup() {
+	global $fractal;
+	$fractal = array();
+	$fractal['crawl'] = false;
+}
+
+add_action( 'template_redirect', 'fractal_template_setup', 1 );
+
+/*
+ *	fractal_block( $block_name, $block_function )
+ *
+ *	@description	Defines a template block and adds it to the $fractal chain
+ *
+ *	@param	$block	The unique name of the block as a string
+ *	@param	$block_closure	A function that generates a string of html for output
+ */
+
+function fractal_block( $block, $block_closure ) {
+	global $fractal;
+	
+	$fractal[$block]['closures'][] = $block_closure;
+	if ( $fractal['crawl'] ) {
+		$output = fractal_crawl( $block );	
+		return $output;
+	}
+}
+
+/*
+ *	fractal_parent()
+ */
+
+function fractal_parent() {
+	global $fractal;
+	
+	if ( ! isset( $fractal['working_block'] ) )
+		return "<p>fractal_parent returned false</p>";
+	$working_block = $fractal['working_block'];
+	if ( ! isset( $fractal[$working_block]['html'] ) )
+		return "<p>fractal_parent returned false</p>";
+	echo $fractal[$working_block]['html'];
+}
+
+/*
+ *	fractal( $fractal_parent_file )
+ *	@description	Template tag called at the end of each fractal template file.
+ *					Handles inheritance starts the chain collapse when at the base
+ *
+ *	@param	$fractal_parent	The parent file this calling file inherits from
+ */
+
+function fractal( $fractal_parent = null ) {
+	global $fractal;
+	
+	// if there is a parent file
+	if ( isset( $fractal_parent ) ) {
+		locate_template( "/fractal/fractal.$fractal_parent.php", true );
+		return true;
+	}
+	
+	// switch to crawl mode;
+	$fractal['crawl'] = true;
+
+	// Start the fractal chain collapse and echo results
+	echo fractal_crawl( 'base' );
+}
+
+/*
+ *	fractal_crawl()
+ *	@description	Crawl up the assembled fractal chain to assemble output and echo it.
+ *
+ *	@param	$block	The block to stitch
+ */
+
+function fractal_crawl( $block ) {
+	global $fractal;
+	$fractal['working_block'] = $block;
+
+	/*echo "\n<p>Called fractal_crawl( '$block' ). Entering with:</p>\n";
+	print_r( $fractal );
+	echo "\n";*/
+	
+	while ( count( $fractal[$block]['closures'] ) > 0 ) {
+		
+		$closure = array_pop( $fractal[$block]['closures'] );
+		if ( is_callable( $closure ) ) {
+			ob_start();
+			call_user_func( $closure );
+			$output = ob_get_contents();
+			ob_end_clean();
+			$fractal[$block]['html'] = $output;
+		}
+		/* echo "\n<p>Through the while loop within fractal_crawl(). fractal is:</p>\n";
+		print_r( $fractal );
+		echo "\n"; */
+	
+	}	
+	echo $fractal[$block]['html'];
+}
+ 
+/*
+ *	Shortcode Setup
+ *
+ *	@todo: Extend support for declaring blocks in post content that are filtered like normal shortcodes on the_content();.
+ */
+
+/*
+ *	fractal_template() is a TO BE IMPLEMENTED function that handles shortcode support
+ *	For now it does nothing, but exists for forward compatibility.
+ */
+ 
+function fractal_template() {
+	
+	return;
+}
+
